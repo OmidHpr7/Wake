f65c2ec5f5b6fb3c90086ad831116bdeed1ef788
Omid Hajipoor <95117342+OmidHpr7@users.noreply.github.com>
2021-12-18 00:06:40 +0330

    Update Main.py

--- Main.py
+++ Main.py
@@ -14,13 +14,38 @@ import Wake
 # read data:
 #   you most read a domain file and text file
 # =============================================================================
-news=open('Data\Sanad2030.txt' , 'r' , encoding='utf8').readlines()
-domainTxt=news
-txt=domainTxt[4]
-numKey=5
+
+cntTxt = open('Data/mydata.txt' , 'r' , encoding='utf8').readlines()
+shortTxt = cntTxt[10]
+
+#initial Param
+numKey=10
 word2vec_param=(5,2)
+
+
+
+# =============================================================================
+# keyword extraction with pretrained model
+# =============================================================================
+
+use_PreTrain_Model = 1
+model_add = 'Model/Model_Wed_Apr__3_16_32_06_2019'
+
+wake = Wake.wake(cntTxt , use_PreTrain_Model, word2vec_param, model_add)
+key = wake.keyword_EXT(shortTxt,numKey)
+
+print(key)
+
+
+'''
+# =============================================================================
+# keyword extraction with traning model
+# =============================================================================
 
 use_PreTrain_Model = 0
 
-wake = Wake.wake(domainTxt , use_PreTrain_Model, word2vec_param)
-key = wake.keyword_EXT(txt,numKey)
+wake = Wake.wake(cntTxt , use_PreTrain_Model, word2vec_param)
+key = wake.keyword_EXT(shortTxt,numKey)
+
+print(key)
+'''
